628ceb4b474223565d18ceb01f0218bf38d41dc8
katiedonkers <36115268+katiedonkers@users.noreply.github.com>
2018-11-27 11:41:26 -0500

    Update Glove_CNN_tuneparams1.py

--- GloVe/Glove_CNN_tuneparams1.py
+++ GloVe/Glove_CNN_tuneparams1.py
@@ -38,6 +38,7 @@ def construct_vector_data(docs, sentiments, embedding):
     train_x = [[embedding.get(word) if word in embedding else np.zeros(25) for word in doc] for doc in docs]
     print('Outputting first tweet ...')
     train_x = keras.preprocessing.sequence.pad_sequences(train_x, maxlen=100, padding='pre', truncating='pre', dtype='float')
+    print('we got past train_x, onto train y...')
     train_y = np.asarray([1 if x==4 else 0 for x in sentiments], dtype=np.int32)
     print("train_x length: ",len(train_x), ", train_y length: ",len(train_y))
     #print(train_x[1])
